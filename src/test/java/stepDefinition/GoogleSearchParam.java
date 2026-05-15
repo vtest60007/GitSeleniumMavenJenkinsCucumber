3b23c56eb60c8ddc917864d8144fb7190e54013f
Venkatesh G Iyengar <vtest6007@gmail.com>
2026-05-15 14:48:23 +0530

    added cucumber simple paramterization

---
+++ src/test/java/stepDefinition/GoogleSearchParam.java
@@ -0,0 +1,38 @@
+package stepDefinition;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+
+import browserSetup.BrowserSetup;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import pages.GoogleSearch;
+import utils.ReadPropeties;
+
+public class GoogleSearchParam {
+	
+	WebDriver driver;
+	ReadPropeties rp= new ReadPropeties();
+	
+	@Given("I am on the Google search page")
+	public void i_am_on_the_google_search_page() throws IOException {
+		BrowserSetup bs=new BrowserSetup();
+		driver=bs.setupBrowser("Chrome");
+
+		driver.get(rp.ReadProperty("./src/test/resources/config.properties", "url"));
+	}
+
+	@When("I search for {string}")
+	public void i_search_for(String keyword) {
+		
+		GoogleSearch searchPage= new GoogleSearch(driver);			
+		searchPage.EnterKeyword(keyword);
+	}
+
+	@Then("I should see search results for {string}")
+	public void i_should_see_search_results_for(String Title) {
+		System.out.println("Search Completed "+Title);
+	}
+}
--- src/test/java/stepDefinition/GoogleSearchSteps.java
+++ src/test/java/stepDefinition/GoogleSearchSteps.java
@@ -16,13 +16,13 @@ public class GoogleSearchSteps {
 	WebDriver driver;
 	ReadPropeties rp= new ReadPropeties();
 	
-	@Given("I am on the Google search page")
-	public void i_am_on_the_google_search_page() throws IOException {
-		BrowserSetup bs=new BrowserSetup();
-		driver=bs.setupBrowser("Chrome");
-
-		driver.get(rp.ReadProperty("./src/test/resources/config.properties", "url"));
-	}
+//	@Given("I am on the Google search page")
+//	public void i_am_on_the_google_search_page() throws IOException {
+//		BrowserSetup bs=new BrowserSetup();
+//		driver=bs.setupBrowser("Chrome");
+//
+//		driver.get(rp.ReadProperty("./src/test/resources/config.properties", "url"));
+//	}
 
 	@When("I search for a Keyword")
 	public void i_search_for_a_keyword() {
@@ -37,4 +37,5 @@ public class GoogleSearchSteps {
 		System.out.println("Search Completed");
 	}
 
+	
 }
